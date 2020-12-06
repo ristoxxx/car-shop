3a67d70965debda0b98481a6162a2af6d0234338
ristoxxx <lahteri@gmail.com>
2020-12-06 10:41:48 +0200

    depencies updated and first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Carlist from './components/Carlist'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>And there was a light!</h1>
+        <Carlist />
       </header>
     </div>
   );
---
+++ src/components/Carlist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Carlist () {
+    return (
+    <div>
+        Carlist
+    </div>
+    );
+}
+
+export default Carlist;

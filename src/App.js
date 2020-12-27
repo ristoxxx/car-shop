b29aca4ce00ea2900d7803f92c827b2109aae3af
ristoxxx <lahteri@gmail.com>
2020-12-27 16:51:50 +0200

    add car välivaihe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Carlist from './components/Carlist'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
-    <div className="App">      
-        <Carlist />
+    <div className="App"> 
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            CarShop
+          </Typography>
+        </Toolbar>      
+      </AppBar>     
+      <Carlist />
     </div>
   );
 }
---
+++ src/components/AddCar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+function AddCar() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return(
+        <div>
+            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Add Car
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle >New Car</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+        </div>
+        )
+}
+
+export default AddCar;
